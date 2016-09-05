32e04f6bec002df1b1c8596e9cdce5e4b91be0a7
nyrobert <nyitrai.robert@gmail.com>
2016-09-05 12:34:55 +0000

    test openid connect lib

--- src/app.php
+++ src/app.php
@@ -6,7 +6,17 @@ use \Psr\Http\Message\ResponseInterface as Response;
 $app = new \Slim\App;
 
 $app->get('/', function (Request $request, Response $response) {
-	$response->getBody()->write('Hello');
+	$oidc = new OpenIDConnectClient(
+		'hhttps://accounts.google.com/o/oauth2/v2/auth',
+		'392849214846-iuqevu02aa5so05ps614kfqce1ggbhc7.apps.googleusercontent.com',
+		'jCXn7SuU_uuoKMTx2e6P7NNw'
+	);
+
+	$oidc->authenticate();
+
+	$email = $oidc->requestUserInfo('email');
+
+	$response->getBody()->write('Hello '.$email);
 
 	return $response;
 });
